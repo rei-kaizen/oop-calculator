3e4874452dd9c660d33cf42cd0cfe4c6d8540092
Rei Kaizen <bondenie@gmail.com>
2023-06-03 13:02:15 +0800

    some modification

--- src/main.py
+++ src/main.py
@@ -2,15 +2,15 @@ from calculation import Calculate
 from retry_function import TryAgain
 
 # create the instance of the Calculate class
-ui1 = Calculate()
+calc = Calculate()
 
 # create the instance of the TryAgain class
-ui2 = TryAgain() 
+attempt = TryAgain() 
 
 # call the calc_function method from  the  ui instance to perform the calculation and print the result
-output = ui1.calculation()
+output = calc.calculation()
 print(f"\033[91mResult:\033[0m {output}")
 print(".\n.\n.")
 
 # call the retry function from the ui instance to allow tthe user to try again
-ui2.retry()
+attempt.retry()
