5cb4bba6bf153e0e674ee85e5752e59ffc391796
Rei Kaizen <bondenie@gmail.com>
2023-06-03 19:56:06 +0800

    add docstring

--- src/output_format.py
+++ src/output_format.py
@@ -2,7 +2,25 @@ from calculation import Calculate
 from fractions import Fraction
 
 class OutputConverter(Calculate):
+    """
+    This class is used to convert the output of a calculation to different formats.
+    
+    Args:
+        Calculate (class): The base class for performing calculations.
+    """
     def output_format(self, output):
+        """
+        Converts the output value to the desired format based on user input.
+
+        Args:
+            output (float): The output value to be converted.
+
+        Returns:
+            str: The converted output value in the selected format.
+
+        Raises:
+            ValueError: If an invalid option is chosen by the user.
+        """
         print("\033[36mChoose the output format:\033[0m ")
         print("\033[35m1. Round off to two decimal places\033[0m")
         print("\033[35m2. Scientific Notation\033[0m")
